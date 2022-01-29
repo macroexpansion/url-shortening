0078d50f99c634a21c9ba85532114a63b62434af
trasua <hyouka266@gmail.com>
2022-01-30 01:24:25 +0700

    update

--- program/src/main.rs
+++ program/src/main.rs
@@ -9,46 +9,46 @@ use crate::redis::{Commands, Connection};
 async fn main() {
     let client = redis::Client::open("redis://127.0.0.1/").unwrap();
     let mut con = client.get_connection().unwrap();
-    let _ : () = con.set("https://google.com", "aaaaaa").unwrap();
 
     let database = Database::new().await.unwrap();
     // database.init().await.unwrap();
+    add_url(&database, "https://google.com").await.unwrap();
 
-    processor(&mut con, &database, "https://google.com").await.unwrap();
+    processor(&mut con, &database, "aaaaab").await.unwrap();
+    processor(&mut con, &database, "aaaaaa").await.unwrap();
+    processor(&mut con, &database, "aaaaaa").await.unwrap();
 }
 
-async fn processor(cache: &mut Connection, database: &Database, url: &str) -> Result<()> {
-    match find_url(cache, &database, url).await {
+async fn processor(cache: &mut Connection, database: &Database, hash: &str) -> Result<()> {
+    match find_url(cache, &database, hash).await {
         Ok(()) => {},
-        Err(error) => {
-            if error.to_string() == "not found" {
-                shorten_url(database, url).await?;
-            } else {
-                println!("{error:?}");
-            }
-        }
+        Err(error) => println!("{error:?}")
     };
 
     Ok(())
 }
 
-async fn find_url(cache: &mut Connection, database: &Database, url: &str) -> Result<()> {
-    match cache.get::<String, String>(url.to_string()) {
+async fn find_url(cache: &mut Connection, database: &Database, hash: &str) -> Result<()> {
+    // find in cache
+    match cache.get::<String, String>(hash.to_string()) {
         Ok(value) => {
-            println!("{value}");
+            println!("cache {value}");
             return Ok(());
         },
         Err(_) => {}
     };
 
-    println!("bug");
-    
-    // database.select("*", "db.urls", &format!("original_url = '{url}'")).await?;
+    // find in database
+    database.select("*", "db.urls", &format!("original_url = '{url}'")).await?;
+
+    // update cache
+    cache.set("aaaaaa", "https://google.com")?;
+    println!("updated cache");
 
     Ok(())
 }
 
-async fn shorten_url(database: &Database, url: &str) -> Result<()> {
+async fn add_url(database: &Database, url: &str) -> Result<()> {
     let hash = key_gen();
     let user_id = 0;
     let url_values = format!("('{hash}', '{url}', {user_id})");
